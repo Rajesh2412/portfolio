5581f6490b38b3c60964b145f95371e04b4e3d27
Rajesh2412 <rajeshramoju1997@gmail.com>
2024-12-17 12:24:04 +0000

    initial Commit

---
+++ scripts/work.js
@@ -0,0 +1,10 @@
+const skillsData = [
+  {name: "Angular",logoURL : "/angular_gradient.png"},
+  {name: "React",logoURL : "/angular_gradient.png"},
+  {name: "Node JS",logoURL : "/angular_gradient.png"},
+  {name: "JavaScript",logoURL : "/angular_gradient.png"},
+  {name: "TypeScript",logoURL : "/angular_gradient.png"},
+  {name: "Go Lang",logoURL : "/angular_gradient.png"},
+  {name: "SQL",logoURL : "/angular_gradient.png"},
+
+]
